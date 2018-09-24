52e60c694f479cd74ae76c4935645475676e91fc
Lam7150 <Lam7150@gmail.com>
2018-09-24 15:23:42 -0500

    completing weigh_pokemons, single_type_candy_count, and reflections_and_projections

--- hw1.py
+++ hw1.py
@@ -9,33 +9,79 @@ import csv
 import numpy as np
 
 def main():
-    histogram_times("airplane_crashes.csv")
+    pyarray = np.array([[1, 1], [2, 2]])
+    reflections_and_projections(pyarray)
 
 def histogram_times(filename):
-    planeCrashTimes = [0] * 24
+    # initializing list to track plane crash times
+    plane_crash_times = [0] * 24
 
+    # opening and loading plane crash history
     with open(filename, "r") as csvfile:
         airplanes = csv.DictReader(csvfile)
+
+        # iterating through planes in plane crash history
         for row in airplanes:
+            
+            # sanitizing input
             if row['Time'] != "":
                 row['Time'] = row['Time'].lstrip("c: ")
 
+                # cleaning and recording plane crash times
                 if (row['Time'][:2] == "00"):
-                    planeCrashHour = 0
+                    plane_crash_hour = 0
                 else:
-                    planeCrashHour = int(row['Time'][:2].lstrip('0').rstrip(".:'")) % 24
-                planeCrashTimes[planeCrashHour] += 1
+                    plane_crash_hour = int(row['Time'][:2].lstrip('0').rstrip(".:'")) % 24
+                plane_crash_times[plane_crash_hour] += 1
 
-    return planeCrashTimes
+    return plane_crash_times
+    
 
 def weigh_pokemons(filename, weight):
-    pass
+    # initializing pokemon list
+    matched_pokemon = []
+
+    # opening and loading pokedex
+    with open(filename, "r") as file:
+        pokedex = json.load(file)
+
+        # iterating through pokedex and recording pokemon with matching weights into pokemon list
+        for pokemon in pokedex["pokemon"]:
+            if float(pokemon["weight"].rstrip(" kg")) == weight:
+                matched_pokemon.append(pokemon["name"])
+
+    return matched_pokemon
+
 
 def single_type_candy_count(filename):
-    pass
+    # initializing candy sum
+    candy_sum = 0
+
+    # opening and loading pokedex
+    with open(filename, "r") as file:
+        pokedex = json.load(file)
+
+        # iterating through pokemon and summing candy count for single-type pokemon
+        for pokemon in pokedex["pokemon"]:
+            if (len(pokemon["type"]) == 1):
+                candy_sum += int(pokemon.get("candy_count", 0))
+
+    return candy_sum
+
 
 def reflections_and_projections(points):
-    pass
+    # reflecting point across y = 1
+    points[1] = (2 - points[1])
+
+    # rotating point 90 degrees about origin
+    temp = np.array(points[0])
+    points[0] = points[1]
+    points[1] = temp
+
+    # projecting point onto line y = 3x
+    points = np.matmul(points, [[1,3], [3,9]])
+    points = np.multiply(points, 0.1)
+
 
 def normalize(image):
     pass
